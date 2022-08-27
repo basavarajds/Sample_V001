cf7266671a72e6d519b59a7f5b025677083b7c98
basavaraj <bdiddege@gmail.com>
2022-08-28 00:20:24 +0530

    Added one new file and updated one file

--- src/test/java/sample/sampleprj/Add_Customer.java
+++ src/test/java/sample/sampleprj/Add_Customer.java
@@ -1,2 +1,4 @@
 //Adding new file
 //Add customer
+//The below code added newly
+//system.out.println("Newly Added code")
---
+++ src/test/java/sample/sampleprj/TC_Search_Customer.java
@@ -0,0 +1,13 @@
+package sample.sampleprj;
+
+import org.testng.annotations.Test;
+
+public class TC_Search_Customer {
+	
+	@Test
+	public void SearchCustomer()
+	{
+		System.out.println("Search Customer");
+	}
+
+}
